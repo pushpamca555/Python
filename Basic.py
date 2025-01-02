ed5536a40c37c697ea3642a9e6c44986ddb0cf1d
pushpamca555 <pushpamca555@gmail.com>
2025-01-02 17:07:06 +0530

    Fisrtcommitt

--- Basic.py
+++ Basic.py
@@ -1,4 +1,15 @@
 print("hello")
 a=5
-b=10
-print(a,b )
+b=15
+if a>b:
+    print("a is big")
+elif b<a:
+    print("b is small")
+elif b>a:
+    print("b is big")
+elif a<b:
+    print("a is small")
+
+else:
+     print("zero")
+print(a,b)
